0072d17e299ec4c8bb473bf631329772ae1a309e
syihabx <122454815+syihabx@users.noreply.github.com>
2023-02-01 17:15:11 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Coba-Remix
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-aeukco)
---
+++ app/routes/notes.tsx
@@ -0,0 +1,7 @@
+export default function Notes() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>Notes</h1>
+    </div>
+  );
+}
